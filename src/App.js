1511b81acd4747a00c28c6594fd74f9cb9454116
[namanmathur007] <85144793+namanmathur007@users.noreply.github.com>
2022-06-02 20:40:03 +0530

    commit on main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
 
 function App() {
   return (
-    <div>
-      Hello World!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/hotels" element={<List/>}/>
+        <Route path="/hotels/:id" element={<Hotel/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
